0bac17351e86b8e86728aca66753834f695b0a25
LPRegen <manuel.escribano.051@gmail.com>
2023-10-25 16:27:39 +0200

    test: extend button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Button } from './Button';
 
 describe('Button comp', () => {
@@ -10,4 +11,18 @@ describe('Button comp', () => {
     const el = screen.getByRole('button');
     expect(el).toBeInTheDocument();
   });
+  it('Executes handler onClick', async () => {
+    const handleClick = jest.fn();
+    const user = userEvent.setup();
+
+    render(<Button onClick={handleClick}>Click me!</Button>);
+
+    const el = screen.getByRole('button', { name: /Click me!/i });
+    await user.click(el);
+
+    expect(handleClick).toHaveBeenCalled();
+  });
+  // TEST: Keyboard interaction
+  // TEST: When it's disabled, handleClick isn't executed.
+  // TEST: Has corresponding role
 });
