af5fd8a243a8bfda2ae88742ff6776bf7cacc93f
LPRegen <manuel.escribano.051@gmail.com>
2023-10-25 21:47:10 +0200

    test: extend test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -6,12 +6,14 @@ describe('Button comp', () => {
   it('Render component', () => {
     render(<Button>Add to cart</Button>);
   });
+
   it('Has correct aria-label', () => {
     render(<Button>Add to cart</Button>);
     const el = screen.getByRole('button');
     expect(el).toBeInTheDocument();
   });
-  it('Executes handler onClick', async () => {
+
+  it('Executes handler onClick w/ click', async () => {
     const handleClick = jest.fn();
     const user = userEvent.setup();
 
@@ -22,7 +24,33 @@ describe('Button comp', () => {
 
     expect(handleClick).toHaveBeenCalled();
   });
+
   // TEST: Keyboard interaction
+  // - on {Enter} and {Space} should be activated
+  // it('Executes handler onClick w/ keyboard', async () => {
+  //   const handleClick = jest.fn();
+  //   const user = userEvent.setup();
+  //   render(<Button onClick={handleClick}>Click me!</Button>);
+  //   const el = screen.getByRole('button', { name: /Click me!/i });
+  //   await user.keyboard('{Enter}');
+  //   expect(el.).toHaveBeenCalled();
+  // });
+
   // TEST: When it's disabled, handleClick isn't executed.
+  it('Doesnt executes handler onClick if btn is disabled', async () => {
+    const handleClick = jest.fn();
+    const user = userEvent.setup();
+
+    render(
+      <Button onClick={handleClick} disabled>
+        Disabled
+      </Button>
+    );
+
+    const el = screen.getByRole('button', { name: /Disabled/i });
+
+    await user.click(el);
+    expect(handleClick).toHaveBeenCalled();
+  });
   // TEST: Has corresponding role
 });
