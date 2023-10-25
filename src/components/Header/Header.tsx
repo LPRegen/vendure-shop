71915c58be42628be7035a4ead29d1cc4d3771cd
LPRegen <manuel.escribano.051@gmail.com>
2023-10-25 22:10:30 +0200

    refactor: add aria-hidden to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ export function Header() {
       <img
         src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png"
         alt="Santex logo"
+        aria-hidden
       />
       <div>$ 0</div>
     </header>
