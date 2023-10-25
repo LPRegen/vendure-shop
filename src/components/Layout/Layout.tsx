fb6223555f149ea2013723ad232b9f813f17c37f
LPRegen <manuel.escribano.051@gmail.com>
2023-10-25 13:00:06 +0200

    refactor: add styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,18 +6,31 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
+const Container = styled(Grid)(({ theme }) => ({
+  backgroundColor: 'black',
+  [theme.breakpoints.down('md')]: {
+    padding: '2rem 1rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    padding: '2rem 2rem',
+  },
+  [theme.breakpoints.up('lg')]: {
+    padding: '3rem 4.5rem',
+  },
+}));
+
 const Item = styled(Paper)({
   backgroundColor: 'grey',
 });
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <Grid container spacing={2}>
+    <Container container spacing={2}>
       {React.Children.map(children, (child, index) => (
         <Grid xs={12} sm={6} md={4} lg={3}>
           <Item key={index}>{child}</Item>
         </Grid>
       ))}
-    </Grid>
+    </Container>
   );
 };
