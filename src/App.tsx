a1e7bf9ccaf4e6071d053d2c34f5d5be223785e3
LPRegen <manuel.escribano.051@gmail.com>
2023-10-25 14:06:18 +0200

    refactor: include Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import * as React from 'react';
 import './App.css';
+import { Layout } from './components/Layout/Layout';
 
 function App() {
-  return <></>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Layout>Card</Layout>
+    </React.Fragment>
+  );
 }
 
 export default App;
