ef340371997b03802099f95654bc748b5198a049
LPRegen <manuel.escribano.051@gmail.com>
2023-10-26 05:51:32 +0200

    refactor: add config apollo & gql

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const commerceLink = createHttpLink({
+  uri: 'https://demo.vendure.io/shop-api/shop-api',
+  headers: {
+    authorization: localStorage.getItem('Auth-Token')
+      ? `Bearer ${localStorage.getItem('Auth-Token')}`
+      : '',
+  },
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    const context = operation.getContext();
+    const authHeader = context.response.headers.get('Vendure-Auth-Token');
+
+    if (authHeader) {
+      localStorage.setItem('Auth-Token', authHeader);
+    }
+    return response;
+  });
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([afterwareLink, commerceLink]),
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
