9dda3f0d8777d7b6ae24a3874a943484e9c44b11
LPRegen <manuel.escribano.051@gmail.com>
2023-10-27 11:53:35 +0200

    refactor: modify interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,15 @@
+interface Asset {
+  source: string;
+  name: string;
+}
+
 interface Variants {
   id: string;
   price: number;
 }
 
 export interface CardProps {
-  featuredAsset: {
-    source: string;
-    name: string;
-  };
+  featuredAsset: Asset;
   name: string;
   description: string;
   variants: Array<Variants>;
