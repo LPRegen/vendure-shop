465e7303d5babf5c370eb4182e583fea0833a54d
LPRegen <manuel.escribano.051@gmail.com>
2023-10-27 09:44:05 +0200

    refactor: modify component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,34 @@
+import styled from 'styled-components';
+import { Order } from '../Order/Order';
+
+const Navbar = styled.nav`
+  background-color: #013884;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100px;
+  width: 100%;
+`;
+
+const Logo = styled.img`
+  height: fit-content;
+  width: 50%;
+  @media screen and (min-width: 600px) {
+    width: unset;
+  }
+`;
+
 export function Header() {
   return (
-    <header style={{ background: 'red' }}>
-      <img
+    <Navbar>
+      <Logo
         src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png"
         alt="Santex logo"
         aria-hidden
       />
-      <div>$ 0</div>
-    </header>
+      <div>
+        <Order customer="manu" total={400} />
+      </div>
+    </Navbar>
   );
 }
