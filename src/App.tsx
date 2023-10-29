0217af5e7b88f8a45a23da92fb93b94b26565c88
LPRegen <manuel.escribano.051@gmail.com>
2023-10-29 06:33:47 +0100

    refactor: include components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import * as React from 'react';
 import './App.css';
+import { Header } from './components/Header/Header';
 import { Layout } from './components/Layout/Layout';
+import { ProducList } from './components/ProductList/ProductList';
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Layout>Card</Layout>
+      <Header />
+      <Layout>
+        <ProducList />
+      </Layout>
     </React.Fragment>
   );
 }
