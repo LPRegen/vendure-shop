402c7dfad8f59cb0b4d2a41addff37f968a19d6d
LPRegen <manuel.escribano.051@gmail.com>
2023-10-29 06:33:14 +0100

    refactor: add ProductList comp

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from '@apollo/client';
+import { GET_PRODUCT_LIST } from '../../graphql/queries';
+import { Card } from '../Card/Card';
+import { ItemsData } from '../../types';
+
+export const ProducList = () => {
+  // FIX: move the useQuery to its own file `ProducList`
+  const { loading, error, data } = useQuery<ItemsData>(GET_PRODUCT_LIST, {});
+
+  if (loading) {
+    return <div>Loading products...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading products</div>;
+  }
+
+  if (!data) {
+    return <div>There is no data available</div>;
+  }
+
+  return (
+    <>
+      {data?.products.items.map(
+        ({ id, featuredAsset, name, description, variants }) => (
+          <Card
+            id={id}
+            variants={variants}
+            key={id}
+            name={name}
+            description={description}
+            featuredAsset={featuredAsset}
+          />
+        )
+      )}
+    </>
+  );
+};
