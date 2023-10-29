cf9a6fc2a8c57282c1895df373693264cbee7e97
LPRegen <manuel.escribano.051@gmail.com>
2023-10-29 22:00:06 +0100

    test: add test

---
+++ src/components/ProductList/ProductList.test.tsx
@@ -0,0 +1,42 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen } from '@testing-library/react';
+import {
+  productListMockError,
+  productListMockSuccess,
+} from '../../mocks/productList';
+import { ProductList } from './ProductList';
+
+describe('ProductList comp', () => {
+  it('renders children', async () => {
+    render(
+      <MockedProvider mocks={productListMockSuccess} addTypename={false}>
+        <ProductList />
+      </MockedProvider>
+    );
+
+    const containerEl = await screen.findAllByTestId('grid-container');
+    expect(containerEl.length).toBe(2);
+  });
+
+  it('informs loading state', () => {
+    render(
+      <MockedProvider mocks={productListMockSuccess} addTypename={false}>
+        <ProductList />
+      </MockedProvider>
+    );
+
+    const loadingMsg = screen.getByText('Loading products...');
+    expect(loadingMsg).toBeInTheDocument();
+  });
+
+  it('informs error state', async () => {
+    render(
+      <MockedProvider mocks={productListMockError} addTypename={false}>
+        <ProductList />
+      </MockedProvider>
+    );
+
+    const errorMsg = await screen.findByText('Error loading products');
+    expect(errorMsg).toBeInTheDocument();
+  });
+});
