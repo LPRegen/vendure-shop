7402f13c58790f6b52732c02627fe9ec919a4ee7
LPRegen <manuel.escribano.051@gmail.com>
2023-10-29 11:57:23 +0100

    refactor: merge Layout's logic into ProductList

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,10 +1,25 @@
 import { useQuery } from '@apollo/client';
+import { styled } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import { GET_PRODUCT_LIST } from '../../graphql/queries';
-import { Card } from '../Card/Card';
 import { ItemsData } from '../../types';
+import { Card } from '../Card/Card';
 
 export const ProducList = () => {
   const { loading, error, data } = useQuery<ItemsData>(GET_PRODUCT_LIST, {});
+
+  const Container = styled(Grid)(({ theme }) => ({
+    backgroundColor: '#0D1537',
+    [theme.breakpoints.down('md')]: {
+      padding: '2rem 1rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      padding: '2rem 2rem',
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding: '3rem 4.5rem',
+    },
+  }));
 
   if (loading) {
     return <div>Loading products...</div>;
@@ -19,19 +34,20 @@ export const ProducList = () => {
   }
 
   return (
-    <>
+    <Container container spacing={2}>
       {data?.products.items.map(
         ({ id, featuredAsset, name, description, variants }) => (
-          <Card
-            id={id}
-            variants={variants}
-            key={id}
-            name={name}
-            description={description}
-            featuredAsset={featuredAsset}
-          />
+          <Grid xs={12} sm={6} md={4} lg={3} key={id}>
+            <Card
+              id={id}
+              variants={variants}
+              name={name}
+              description={description}
+              featuredAsset={featuredAsset}
+            />
+          </Grid>
         )
       )}
-    </>
+    </Container>
   );
 };
