df6bb8142cee6e7560007224293cdb14b769bbdd
LPRegen <manuel.escribano.051@gmail.com>
2023-11-05 10:21:27 +0100

    refactor: add functionality

--- src/hooks/useStateWithStorage.tsx
+++ src/hooks/useStateWithStorage.tsx
@@ -1,4 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
 export default function useStateWithStorage(
-  key: string,
-  defaultValue: unknown
-) {}
+  key: string
+): [number, React.Dispatch<React.SetStateAction<number>>] {
+  const storedValue = localStorage.getItem('accountTotal');
+  const initialValue = storedValue ? JSON.parse(storedValue) : 0;
+
+  const [state, setState] = useState<number>(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
